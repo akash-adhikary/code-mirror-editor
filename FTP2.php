123219dcf4cc46c2574bcc18efc429901f308ead
Akash Adhikary <61286006+akash-adhikary@users.noreply.github.com>
2020-08-25 02:03:52 +0530

    Create FTP2.php
    original ftp2 with file name as text

---
+++ FTP2.php
@@ -0,0 +1,92 @@
+<?php
+$files = glob('/home/tr1083170/public_html/code-editor/*php');
+//foreach($files as $file) { echo "filename:".$file."<br />"; }
+
+function select_file()
+{
+$files = glob('/home/tr1083170/public_html/code-editor/*php');
+ foreach($files as $file)
+            {
+				$pattern = '/\/home\/tr1083170\/public_html\/code-editor\//i';
+				$file=preg_replace($pattern, '', $file);
+                echo" <option value=" . "'" .$file ."'". ">".$file. "</option>";
+            }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<title>FTP2</title>
+<style>
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+textarea, select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+div {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
+<body>
+
+<div>
+  <form method='post'>
+	<label for="filename">File Name</label>
+    <input type='text' name='filename'>
+	<label for="code">Code</label>
+	<br>
+    <textarea  name="code" rows="25" cols="120"> </textarea>
+	
+    <input type="submit" name="submit" value="Submit">
+  </form>
+</div>
+
+</body>
+</html>
+
+
+<?php
+
+if (isset($_REQUEST['submit']))
+{
+$myfile = fopen($_REQUEST['filename'], "w") or die("Unable to open file!");
+$txt = $_REQUEST['code'];
+fwrite($myfile, $txt);
+fclose($myfile);
+ 
+}
+  
+?>
